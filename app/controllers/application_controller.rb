abcfb9248ff8e7a42abc5e414bfb41471a55fb38
yaguch1 <128358148+yaguch1@users.noreply.github.com>
2023-05-14 21:48:57 +0900

    Basic認証

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,17 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :basic_auth
 
   protect_from_forgery with: :null_session
 
   private
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+  end
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'admin' && password == '2222'
+    end
   end
 end
